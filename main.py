5d6b6dd31d364990179ad76787145f9ae4650f38
Shreyansh Kushwaha <shreyansh.kushw@gmail.com>
2026-01-18 18:55:47 +0530

    Added the game ending logic and completing a normal human vs human tic tac toe

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from tkinter import ttk
-
+from collections import deque
 from tkinter import *
-# from tkinter.ttk import *
+from pyautogui import confirm, alert
+import os
 
 class GUI:
 
@@ -9,47 +9,147 @@ class GUI:
         
         self.root = Tk()
         self.root.title("Vanishing Tic-Tac-Toe")
-        self.root.geometry("400x483")
+        self.root.geometry("461x647")
         self.root.resizable(False, False)
 
         self.square_frame = Frame(self.root, width=100, height=100)
         self.square_frame.grid(column=0, row=0)
 
         # Creating a button inside this frame
-        btn00 = Button(self.square_frame, text ="", bg = "white")
-        btn00.config(height = 10, width = 18)
-        btn00.grid(column = 0, row = 0)
+        self.btn00 = Button(self.square_frame, text ="", bg = "white")
+        self.btn00.config(height = 10, width = 18)
+        self.btn00.grid(column = 0, row = 0)
 
-        btn01 = Button(self.square_frame, text ="", bg = "white")
-        btn01.config(height = 10, width = 18)
-        btn01.grid(column = 15, row = 0)
+        self.btn01 = Button(self.square_frame, text ="", bg = "white")
+        self.btn01.config(height = 10, width = 18)
+        self.btn01.grid(column = 15, row = 0)
 
-        btn02 = Button(self.square_frame, text ="", bg = "white")
-        btn02.config(height = 10, width = 18)
-        btn02.grid(column = 30, row = 0)
+        self.btn02 = Button(self.square_frame, text ="", bg = "white")
+        self.btn02.config(height = 10, width = 18)
+        self.btn02.grid(column = 30, row = 0)
 
-        btn10 = Button(self.square_frame, text ="", bg = "white")
-        btn10.config(height = 10, width = 18)
-        btn10.grid(column = 0, row = 10)
+        self.btn10 = Button(self.square_frame, text ="", bg = "white")
+        self.btn10.config(height = 10, width = 18)
+        self.btn10.grid(column = 0, row = 10)
 
-        btn11 = Button(self.square_frame, text ="", bg = "white")
-        btn11.config(height = 10, width = 18)
-        btn11.grid(column = 15, row = 10)
+        self.btn11 = Button(self.square_frame, text ="", bg = "white")
+        self.btn11.config(height = 10, width = 18)
+        self.btn11.grid(column = 15, row = 10)
 
-        btn12 = Button(self.square_frame, text ="", bg = "white")
-        btn12.config(height = 10, width = 18)
-        btn12.grid(column = 30, row = 10)
-        btn20 = Button(self.square_frame, text ="", bg = "white")
-        btn20.config(height = 10, width = 18)
-        btn20.grid(column = 0, row = 20)
+        self.btn12 = Button(self.square_frame, text ="", bg = "white")
+        self.btn12.config(height = 10, width = 18)
+        self.btn12.grid(column = 30, row = 10)
 
-        btn21 = Button(self.square_frame, text ="", bg = "white")
-        btn21.config(height = 10, width = 18)
-        btn21.grid(column = 15, row = 20)
+        self.btn20 = Button(self.square_frame, text ="", bg = "white")
+        self.btn20.config(height = 10, width = 18)
+        self.btn20.grid(column = 0, row = 20)
 
-        btn22 = Button(self.square_frame, text ="", bg = "white")
-        btn22.config(height = 10, width = 18)
-        btn22.grid(column = 30, row = 20)
-        self.root.mainloop()
+        self.btn21 = Button(self.square_frame, text ="", bg = "white")
+        self.btn21.config(height = 10, width = 18)
+        self.btn21.grid(column = 15, row = 20)
 
-tester = GUI()
+        self.btn22 = Button(self.square_frame, text ="", bg = "white")
+        self.btn22.config(height = 10, width = 18)
+        self.btn22.grid(column = 30, row = 20)
+        
+
+class GameState:
+
+    def __init__(self):
+        self.winning_scenerio = [
+            [(0,0),(0,1),(0,2)],
+            [(1,0),(1,1),(1,2)],
+            [(2,0),(2,1),(2,2)],
+            [(0,0),(1,0),(2,0)],
+            [(0,1),(1,1),(2,1)],
+            [(0,2),(1,2),(2,2)],
+            [(0,0),(1,1),(2,2)],
+            [(0,2),(1,1),(2,0)]
+        ]
+
+        self.board = {
+            (0,0) : " ",
+            (0,1) : " ",
+            (0,2) : " ",
+            (1,0) : " ",
+            (1,1) : " ",
+            (1,2) : " ",
+            (2,0) : " ",
+            (2,1) : " ",
+            (2,2) : " ",
+        }
+
+        self.Player_X = "X"
+        self.Player_O = "O"
+        
+        self.ui = GUI()
+        self.update_board()
+        
+        self.Player_X_queue = deque()
+        self.Player_O_queue = deque()
+
+        self.plays = 0
+        self.current_player = "X" 
+
+    def update_board(self):
+
+        self.ui.btn00.config(text=self.board[(0,0)], command= lambda: self.play((0,0)))
+        self.ui.btn01.config(text=self.board[(0,1)], command= lambda: self.play((0,1)))
+        self.ui.btn02.config(text=self.board[(0,2)], command= lambda: self.play((0,2)))
+        self.ui.btn10.config(text=self.board[(1,0)], command= lambda: self.play((1,0)))
+        self.ui.btn11.config(text=self.board[(1,1)], command= lambda: self.play((1,1)))
+        self.ui.btn12.config(text=self.board[(1,2)], command= lambda: self.play((1,2)))
+        self.ui.btn20.config(text=self.board[(2,0)], command= lambda: self.play((2,0)))
+        self.ui.btn21.config(text=self.board[(2,1)], command= lambda: self.play((2,1)))
+        self.ui.btn22.config(text=self.board[(2,2)], command= lambda: self.play((2,2)))
+        
+
+    def play(self, position:tuple[int]):
+        print(self.current_player)
+        if self.current_player == "X":
+            self.Player_X_queue.append(position)
+            
+            if self.board[position] == " ":
+                self.board[position] = self.current_player
+            else:
+                pass
+            
+            self.current_player = "O"
+            self.update_board()
+            self.game_state_check()
+        else:
+            self.Player_O_queue.append(position)
+            
+            if self.board[position] == " ":
+                self.board[position] = self.current_player
+            else:
+                pass   
+            self.current_player = "X"
+            self.update_board()
+            self.game_state_check()
+
+    def game_state_check(self):
+
+        for lines in self.winning_scenerio:
+            if self.board[lines[0]] == self.board[lines[1]] == self.board[lines[2]] and self.board[lines[0]] != " ":
+                self.game_over(f"{self.board[lines[0]]}")
+                return
+            else:
+                pass
+
+        if all(list(map(lambda x: x.strip(), self.board.values()))):
+            self.update_board()
+            self.game_over("tie")
+
+    def game_over(self, winner:str):
+        if not winner == "tie":
+            alert(text=f"Game Over! {winner} wins!", title="Game Over", button="OK")
+        
+        else:
+            alert(text="Game Over! Match Tied!", title="Game Over", button="OK")
+        
+        os._exit(0)
+
+tester = GameState()
+
+tester.ui.root.mainloop()
